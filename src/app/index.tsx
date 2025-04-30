e9e35011a1e602a4d3af40d255cce3507d12443f
louisrubin <miguel10.rbn@gmail.com>
2025-04-30 20:03:02 -0300

    carpetas reubicadas

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import LoginScreen from "./(auth)/Login";
+
+export default function Index() {
+    return (
+        <LoginScreen />
+    )
+}
---
+++ src/constants/imagePath.ts
@@ -0,0 +1,6 @@
+export default {
+    logoGobChaco: require('../assets/images/logo-gobierno-chaco.png'),
+    logoICCH: require('../assets/images/logo-icch.png'),
+    iconLogin: require('../assets/images/Login.png'),
+    // iconLogout: require('../assets/images/Logout.png'),
+}
