5d20d5cde7298d9a3210a9c79a6573b0792c52f3
louisrubin <miguel10.rbn@gmail.com>
2025-05-20 15:36:33 -0300

    status 'Verificado'

--- src/app/(main)/UserSettings.tsx
+++ src/app/(main)/UserSettings.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import ButtonX from '../../components/ButtonX'
 import { useRouter } from 'expo-router';
@@ -32,81 +32,86 @@ export default function UserSettings() {
     }
     return (
         <SafeAreaProvider style={styles.container}>
-            <View style={styles.header}>
-                <Image source={imagePath.userLogo} style={styles.userHeaderIcon} />
-                <Text style={{fontSize: 28}}>
-                    Detalles de la cuenta
-                </Text>
-            </View>
+            <ScrollView>
+                <View style={styles.header}>
+                    <Image source={imagePath.userLogo} style={styles.userHeaderIcon} />
+                    <Text style={{fontSize: 28}}>
+                        Detalles de la cuenta
+                    </Text>
+                </View>
 
-            <View style={styles.body}>
-                <InputAndLabelX titleLabel='Nombre' editable={false}
-                styleLabel={{opacity: 0.4}} value={userData?.nombre}
-                bgColorInput={"#D9D9D9"}
-                colorTexto='#757575'
-                />
+                <View style={styles.body}>
+                    <InputAndLabelX titleLabel='Nombre' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.nombre}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
 
-                <InputAndLabelX titleLabel='Apellido' editable={false}
-                styleLabel={{opacity: 0.4}} value={userData?.apellido}
-                bgColorInput={"#D9D9D9"}
-                colorTexto='#757575'
-                />
-                
-                <InputAndLabelX titleLabel='DNI' editable={false}
-                styleLabel={{opacity: 0.4}} value={userData?.dni}
-                bgColorInput={"#D9D9D9"}
-                colorTexto='#757575'
-                />
-                
-                <InputAndLabelX titleLabel='Correo Electrónico' editable={false}
-                styleLabel={{opacity: 0.4}} value={userData?.email}
-                bgColorInput={"#D9D9D9"}
-                colorTexto='#757575'
-                />
-                
-                <InputAndLabelX titleLabel='Fecha de Registro' editable={false}
-                styleLabel={{opacity: 0.4}} value={userData?.fechaRegistro}
-                bgColorInput={"#D9D9D9"}
-                colorTexto='#757575'
-                />
+                    <InputAndLabelX titleLabel='Apellido' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.apellido}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
+                    
+                    <InputAndLabelX titleLabel='DNI' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.dni}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
+                    
+                    <InputAndLabelX titleLabel='Correo Electrónico' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.email}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
+                    
+                    <InputAndLabelX titleLabel='Fecha de Registro' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.fechaRegistro}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
 
+                    <InputAndLabelX titleLabel='Verificado' editable={false}
+                    styleLabel={{opacity: 0.4}} value={userData?.aprobado ? "Sí" : "No"}
+                    bgColorInput={"#D9D9D9"}
+                    colorTexto='#757575'
+                    />
 
-                <ButtonX 
-                    iconParam={imagePath.arrowLeftLogo} 
-                    iconPosition="left" 
-                    onPress={ () => router.back() }
-                    fontSize={moderateScale(20)}
-                    buttonStyles={styles.button}
-                    bgColor='#ECF692'
-                    bgColorPressed='#CAD561'
-                >
-                    Volver
-                </ButtonX>
+                    <ButtonX 
+                        iconParam={imagePath.arrowLeftLogo} 
+                        iconPosition="left" 
+                        onPress={ () => router.back() }
+                        fontSize={moderateScale(20)}
+                        buttonStyles={styles.button}
+                        bgColor='#ECF692'
+                        bgColorPressed='#CAD561'
+                    >
+                        Volver
+                    </ButtonX>
 
-                <ButtonX 
-                    iconParam={imagePath.iconLogout} 
-                    iconPosition="left" 
-                    fontSize={moderateScale(20)}
-                    buttonStyles={styles.button}
-                    bgColor='#F9B9B2'
-                    bgColorPressed='#F69C92'
-                    onPress={ handleLogOut }
-                >
-                    Cerrar Sesión
-                </ButtonX>
+                    <ButtonX 
+                        iconParam={imagePath.iconLogout} 
+                        iconPosition="left" 
+                        fontSize={moderateScale(20)}
+                        buttonStyles={styles.button}
+                        bgColor='#F9B9B2'
+                        bgColorPressed='#F69C92'
+                        onPress={ handleLogOut }
+                    >
+                        Cerrar Sesión
+                    </ButtonX>
 
-            </View>
+                </View>
 
+            </ScrollView>
         </SafeAreaProvider>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: "center",
-        justifyContent: "center",
         backgroundColor: "#feb", // #feb
-        paddingVertical: moderateVerticalScale(30),
+        paddingTop: moderateVerticalScale(25),
     },
     header:{
         alignItems: "center",
@@ -116,12 +121,15 @@ const styles = StyleSheet.create({
     body: {
         flexGrow: 1,
         marginHorizontal: moderateScale(25),
+        marginBottom: moderateScale(40),
+
         paddingHorizontal: moderateScale(20),
-        paddingVertical: moderateScale(5),
+        paddingBottom: moderateVerticalScale(25),
+        
         backgroundColor: "#fed",
         borderWidth: 1,
         borderRadius: 16,
-        width: "85%",
+        // width: "85%",
         
         borderColor: '#d7d7d7',
     },
