74636b485e07554acffe83bd2384e86555745814
louisrubin <miguel10.rbn@gmail.com>
2025-06-07 00:29:41 -0300

    update lista preguntas

--- src/app/encuesta/index.tsx
+++ src/app/encuesta/index.tsx
@@ -13,7 +13,7 @@ import { format } from "date-fns";
 import { router } from "expo-router";
 import ModalX from "../../components/Modal";
 import { useUser } from "../../hooks/UserContext";
-import { preguntas, preguntasPorPagina, titulosPorPagina } from "../../constants/PreguntasInfo";
+import { preguntas } from "../../constants/PreguntasInfo";
 import { getFirebaseErrorMessage } from "../../hooks/firebaseHooks";
 import FotosDNI from "../fotos";
 
@@ -23,7 +23,11 @@ export default function Encuestas() {
     const [pagina, setPagina] = useState(0);
     // un objeto con claves tipo string y valores que pueden ser string o Date
     const [respuestas, setRespuestas] = useState<{ [key: string]: string | Date }>(
-        Object.fromEntries(preguntas.map(p => [p.key, ""]))
+      () => Object.fromEntries(
+         preguntas
+         .flatMap(seccion => seccion.campos)
+         .map(p => [p.key, ""])
+      )
     );
     const [urlsFotos, setUrlsFotos] = useState<(string | null)[]>([null, null, null, null]);
 
@@ -94,20 +98,11 @@ export default function Encuestas() {
         }
     };
 
-    const getPreguntasPagina = () => {
-        let start = 0;
-        for (let i = 0; i < pagina; i++) {
-            start += preguntasPorPagina[i];
-        }
-        const end = start + preguntasPorPagina[pagina];
-        return preguntas.slice(start, end);
-    };
-
     const BotonesNavegacion = (
         <View style={styles.footer}>
             <ButtonX
                 onPress={
-                    pagina === preguntasPorPagina.length - 1
+                    pagina === preguntas.length - 1
                         ? () => { abrirConfimSubmitModal()
                          }
                         : () => setPagina(pagina + 1)
@@ -126,7 +121,7 @@ export default function Encuestas() {
                 }}
                 disabled={false}
             >
-                {pagina === preguntasPorPagina.length - 1 ? "Enviar" : "Siguiente"}
+                {pagina === preguntas.length - 1 ? "Enviar" : "Siguiente"}
             </ButtonX>
 
             <ButtonX
@@ -207,123 +202,108 @@ export default function Encuestas() {
             {/* HEADER */}
             <View style={styles.header}>
                 <Text style={styles.title}>
-                    {titulosPorPagina[pagina] || "Encuesta"}
+                    {preguntas[pagina].titulo || "Encuesta"}
                 </Text>
                 
                 <Text style={styles.paginador}>
-                    Página {pagina + 1} / {preguntasPorPagina.length}
+                    Página {pagina + 1} / {preguntas.length}
                 </Text>
             </View>
 
-            {/* BODY */}
-            {/* <KeyboardAvoidingView
-                style={{ flex: 1 }}
-                behavior={Platform.OS === "ios" ? "padding" : "height"}
-                keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
-            > */}
-                <LinearGradient colors={["#fff", "#ddc", ]} style={{flex: 1}}>
-                    <View style={{flex: 1}}>
-                    <ScrollView contentContainerStyle={{flexGrow: 1}}>
+            <LinearGradient colors={["#fff", "#ddc", ]} style={{flex: 1}}>
+                <View style={{flex: 1}}>
+                <ScrollView contentContainerStyle={{flexGrow: 1}}>
 
-                        {/* HEADER */}
-                        {/* <View style={styles.header}>
-                            <Text style={styles.title}>
-                                {titulosPorPagina[pagina] || "Encuesta"}
-                            </Text>
-                            
-                            <Text style={styles.paginador}>
-                                Página {pagina + 1} / {preguntasPorPagina.length}
-                            </Text>
-                        </View> */}
+                    <View style={styles.innerContainer}>
+                        <View style={styles.card}>
+                            {pagina < preguntas.length -1 ? (
+                                preguntas[pagina].campos.map((campo) => (
 
-                        <View style={styles.innerContainer}>
-                            <View style={styles.card}>
-                                {pagina < preguntasPorPagina.length - 1 ? (
-                                    getPreguntasPagina().map((pregunta) => (
-                                        <View key={pregunta.key} style={{ marginBottom: 20 }}>
-                                            <Text style={styles.label}>{pregunta.label}</Text>
-                                            {/* ...inputs según tipo... */}
-                                            {pregunta.type === "select" ? (
-                                                <View style={{ borderWidth: 1, borderColor: "#ddd", borderRadius: 8, overflow: "hidden" }}>
-                                                    <Picker
-                                                        selectedValue={respuestas[pregunta.key]}
-                                                        onValueChange={value => setRespuestas({ ...respuestas, [pregunta.key]: value })}
-                                                        style={{ backgroundColor: "#fff"}}
-                                                    >
-                                                    <Picker.Item label="Seleccione..." value="" />
-                                                        {pregunta.options.map(opt => (
-                                                            <Picker.Item key={opt} label={opt} value={opt} />
-                                                        ))}
-                                                    </Picker>
-                                                </View>
-                                            ) : pregunta.type === "date" ? (
-                                                <>
-                                                <ButtonX
-                                                    onPress={() => setShowDatePicker(pregunta.key)}
-                                                    buttonStyles={{ paddingVertical: 8, borderRadius: 30, }}
-                                                    fontSize={18}
-                                                    bgColor="#ddd"
-                                                    bgColorPressed="#BFBFBF"
-                                                >
-                                                    {
-                                                    respuestas[pregunta.key] instanceof Date
-                                                        ? format(respuestas[pregunta.key] as Date, "dd / MM / yyyy")
-                                                        : "Seleccionar fecha"}
-                                                </ButtonX>
+                                    <View key={campo.key} style={{ marginBottom: 20 }}>
+                                        <Text style={styles.label}>{campo.label}</Text>
+                                       {
+                                       campo.type === "date" ? (
+                                          <>
+                                          <ButtonX
+                                                onPress={() => setShowDatePicker(campo.key)}
+                                                buttonStyles={{ paddingVertical: 8, borderRadius: 30, }}
+                                                fontSize={18}
+                                                bgColor="#ddd"
+                                                bgColorPressed="#BFBFBF"
+                                          >
+                                                {
+                                                respuestas[campo.key] instanceof Date
+                                                   ? format(respuestas[campo.key] as Date, "dd / MM / yyyy")
+                                                   : "Seleccionar fecha"}
+                                          </ButtonX>
 
-                                                {/* Mostrar el DateTimePicker si showDatePicker coincide con la pregunta actual  */}
-                                                {showDatePicker === pregunta.key && (
-                                                    <DateTimePicker
-                                                        value={
-                                                            respuestas[pregunta.key] instanceof Date
-                                                            ? respuestas[pregunta.key] as Date
-                                                            : new Date()
-                                                        }
-                                                        mode="date"
-                                                        display="default"
-                                                        minimumDate={new Date(1900, 0, 1)} // Fecha mínima
-                                                        maximumDate={new Date()}
-                                                        onChange={(event, selectedDate) => {
-                                                            setShowDatePicker(null); // Cerrar el picker
+                                          {/* Mostrar el DateTimePicker si showDatePicker coincide con la pregunta actual  */}
+                                          {showDatePicker === campo.key && (
+                                                <DateTimePicker
+                                                   value={
+                                                      respuestas[campo.key] instanceof Date
+                                                      ? respuestas[campo.key] as Date
+                                                      : new Date()
+                                                   }
+                                                   mode="date"
+                                                   display="default"
+                                                   minimumDate={new Date(1900, 0, 1)} // Fecha mínima
+                                                   maximumDate={new Date()}
+                                                   onChange={(event, selectedDate) => {
+                                                      setShowDatePicker(null); // Cerrar el picker
 
-                                                            if (event.type === "set" && selectedDate) {
-                                                                setRespuestas({
-                                                                    ...respuestas,
-                                                                    [pregunta.key]: selectedDate,   // Guardar la fecha seleccionada
-                                                                });
-                                                            }
-                                                            // Si se cancela, no hacer nada y dejar el valor como está
-                                                        }}
-                                                    />
-                                                )}
-                                                </>
-                                            ) : (
-                                                <InputX
-                                                    value={respuestas[pregunta.key] as string}
-                                                    onChangeText={text => setRespuestas({ ...respuestas, [pregunta.key]: text })}
-                                                    placeholder="Respuesta"
-                                                    tipoTeclado={(pregunta.keyboard || "default") as "default" | "email-address" | "number-pad"}
+                                                      if (event.type === "set" && selectedDate) {
+                                                            setRespuestas({
+                                                               ...respuestas,
+                                                               [campo.key]: selectedDate,   // Guardar la fecha seleccionada
+                                                            });
+                                                      }
+                                                      // Si se cancela, no hacer nada y dejar el valor como está
+                                                   }}
                                                 />
-                                            )}
-                                        </View>
-                                    ))
-                                ) : (
-                                    <View style={{ flex: 1, justifyContent: "center" }}>
-                                        <FotosDNI onFotosSubidas={setUrlsFotos} />
+                                          )}
+                                          </>
+                                       )
+                                       : campo.type === "select" ? (
+                                          <View style={{ borderWidth: 1, borderColor: "#ddd", borderRadius: 8, overflow: "hidden" }}>
+                                             <Picker
+                                                   selectedValue={respuestas[campo.key]}
+                                                   onValueChange={value => setRespuestas({ ...respuestas, [campo.key]: value })}
+                                                   style={{ backgroundColor: "#fff"}}
+                                             >
+                                             <Picker.Item label="Seleccione..." value="" />
+                                                   {campo.options.map(opt => (
+                                                      <Picker.Item key={opt} label={opt} value={opt} />
+                                                   ))}
+                                             </Picker>
+                                          </View>
+                                       )
+                                       : (
+                                          <InputX
+                                             value={respuestas[campo.key] as string}
+                                             onChangeText={text => setRespuestas({ ...respuestas, [campo.key]: text })}
+                                             placeholder="Respuesta"
+                                             tipoTeclado={(campo.keyboard || "default") as "default" | "email-address" | "number-pad"}
+                                          />
+                                    )}
+
                                     </View>
-                                )}
-                            </View>
-                            
-                            { BotonesNavegacion }
+                                ))
+                            ) : (
+                                <View style={{ flex: 1, justifyContent: "center" }}>
+                                    <FotosDNI onFotosSubidas={setUrlsFotos} />
+                                </View>
+                            )}
+                        </View>
+                        
+                        { BotonesNavegacion }
 
-                        </View>                        
+                    </View>                        
 
-                    </ScrollView>
-                    </View>
+                </ScrollView>
+                </View>
 
-                </LinearGradient>
-            {/* </KeyboardAvoidingView> */}
-
+            </LinearGradient>
         </SafeAreaView>
        
         </>
